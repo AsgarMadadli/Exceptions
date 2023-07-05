1ea126b99d4ba688e898600e04a887753a4a1415
AsgarMadadli <asgarmm@code.edu.az>
2023-07-05 06:26:11 -0700

    task completed

---
+++ ConsoleApp1/ConsoleApp1/Program.cs
@@ -0,0 +1,58 @@
+﻿using System;
+
+public class ShortEmailException : Exception
+{
+    public ShortEmailException(string message) : base(message)
+    {
+    }
+}
+
+public class NotAnEmailAddressException : Exception
+{
+    public NotAnEmailAddressException(string message) : base(message)
+    {
+    }
+    
+    
+}
+
+public class EmailValidator
+{
+    public bool Validate(string email)
+    {
+        if (email.Length < 10)
+        {
+            throw new ShortEmailException("Email address is too short.");
+        }
+
+        if (!email.EndsWith("@mail.com"))
+        {
+            throw new NotAnEmailAddressException("Not a valid email address.");
+        }
+
+        return true;
+    }
+}
+
+public class Program
+{
+    public static void Main()
+    {
+        EmailValidator validator = new EmailValidator(); 
+
+        try
+        {
+            string email = "ali.aliyev@mail.com";
+            bool isValid = validator.Validate(email);
+            Console.WriteLine("Email is valid: " + isValid);
+        }
+        catch (ShortEmailException ex)
+        {
+            Console.WriteLine("ShortEmailException: " + ex.Message);
+        }
+        catch (NotAnEmailAddressException ex)
+        {
+            Console.WriteLine("NotAnEmailAddressException: " + ex.Message);
+        }
+    }
+}
